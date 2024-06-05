dfa7c859f889d330ed3ecb128b959a2db38abb7b
LU-DUPENG <ludupeng01@gmail.com>
2024-06-05 15:54:41 +0900

    2024/06/05, Lu, Delete unecessary libraries

--- dataflow/create_flex_template/read_from_storage_write_to_bigquery/read_from_cs_write_to_bq.py
+++ dataflow/create_flex_template/read_from_storage_write_to_bigquery/read_from_cs_write_to_bq.py
@@ -1,6 +1,5 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.options.pipeline_options import GoogleCloudOptions
 from apache_beam.io.gcp.bigquery import BigQueryDisposition
 
 
